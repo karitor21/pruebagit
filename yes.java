c399e74510c5326f8f105eb93abae1758113044c
karitor21 <mateusk.0912@gmail.com>
2024-10-04 22:43:38 -0500

    Subida dos

--- yes.java
+++ yes.java
@@ -1,5 +1,5 @@
 public class yes {
     public static void main(String[] args) {
-        
+        System.out.println("quiero que se actualice, como prueba de llo dejo este mensaje aqui");
     }
 }
